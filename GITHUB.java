0cc3f5aac46211e8b7a43033c05f4374ab80fc57
AniruddhaPandit <117506613+AniruddhaPandit@users.noreply.github.com>
2022-11-05 16:26:36 +0530

    learned creating files

--- GITHUB.java
+++ GITHUB.java
@@ -2,5 +2,6 @@ class github{
     public static void main(String args[]){
         System.out.println("Welcome to Github");  
         System.out.println("Learning first Tutorial of Github");
+        System.out.println("COMPLETED Creating file");
   }
 }
