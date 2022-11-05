24f3ca6df02163c806afdf2a6477fb9f8a241d5c
AniruddhaPandit <117506613+AniruddhaPandit@users.noreply.github.com>
2022-11-05 16:23:52 +0530

    GITHUB.java created

---
+++ GITHUB.java
@@ -0,0 +1,6 @@
+class github{
+    public static void main(String args[]){
+        System.out.println("Welcome to Github");  
+        System.out.println("Learning first Tutorial of Github");
+  }
+}
